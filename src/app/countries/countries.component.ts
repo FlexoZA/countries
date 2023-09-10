194fdb25424df142915c74013b2f048dd8adaa0b
Christiaan <christiaan@schoeman.me>
2023-09-10 15:02:55 +0200

    Getting data from API

--- src/app/countries/countries.component.ts
+++ src/app/countries/countries.component.ts
@@ -1,36 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../services/countries.service';
+
+interface CountryData {
+  name: {
+    common: string;
+  };
+  flags?: {
+    svg: string;
+  };
+  flagUrl?: string;
+}
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.css'],
 })
-export class CountriesComponent {
-  itemsPerPage: number = 5; // Default items per page
+export class CountriesComponent implements OnInit {
+  countryData: CountryData[] = [];
 
-  // Replace this array with your actual data source
-  data: any[] = Array.from({ length: 100 }, (_, i) => ({
-    id: i + 1,
-    name: `Item ${i + 1}`,
-  }));
+  constructor(private countriesService: CountriesService) {}
 
-  // Initialize the current page
-  currentPage: number = 1;
-
-  // Function to handle the change in items per page
-  onItemsPerPageChange(): void {
-    this.currentPage = 1; // Reset to the first page when items per page changes
+  ngOnInit() {
+    this.getCountries();
   }
 
-  // Function to get the paginated data based on current page and items per page
-  getPaginatedData(): any[] {
-    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
-    const endIndex = startIndex + this.itemsPerPage;
-    return this.data.slice(startIndex, endIndex);
-  }
+  async getCountries() {
+    try {
+      const response = await this.countriesService.get('all');
+      const responseData = response.data;
 
-  // Function to handle page change
-  onPageChange(event: any): void {
-    this.currentPage = event.pageIndex + 1;
+      // Filter and validate the data before mapping
+      this.countryData = responseData
+        .filter(
+          (item: any) =>
+            item.name && item.name.common && item.flags && item.flags.svg
+        )
+        .map((item: any) => ({
+          name: item.name.common,
+          flagUrl: item.flags.svg,
+        }))
+        .sort((a: { name: string }, b: { name: string }) =>
+          a.name.localeCompare(b.name)
+        ); // Sort alphabetically
+
+      console.log(this.countryData);
+    } catch (error) {
+      console.error('Error:', error);
+    }
   }
 }
---
+++ src/app/services/countries.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import axios from 'axios';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CountriesService {
+  private baseUrl = 'https://restcountries.com/v3.1';
+
+  constructor() {}
+
+  // GET request
+  get(endpoint: string) {
+    const url = `${this.baseUrl}/${endpoint}`;
+    return axios.get(url);
+  }
+}
